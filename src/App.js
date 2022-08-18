4dac61e4fc7a81d7f44feb5cbe826a198877294e
toglesby <oglesbytl@gmail.com>
2022-08-18 04:24:11 -0400

    React test project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [number, setNumber] = React.useState(0);
+  const [textValue, setTextValue] = React.useState("");
+  const [formStage, setFormStage] = React.useState(0);
+  const [names, setNames] = React.useState(["Trevor"])
+  
+  let content; 
+  if (formStage === 0) {
+    content = (
+      <>
+      <h1>Let's Use State</h1>
+      <input onChange={(e) => setTextValue(e.target.value)} type="text" />
+      {textValue}
+      {textValue.length > 10 && <div>Input too long</div>}
+      <button onClick={() => setFormStage(formStage + 1)}>Next</button>
+      </>
+    );
+  }
 
-export default App;
+  if (formStage === 1) {
+    content = (
+      <>
+      <h1>Step 2</h1>
+      <h2>{number}</h2>
+      <button onClick={() => setNumber (number + 1)}>+</button>
+      <button onClick={() => setFormStage(formStage - 1)}>Prev</button>
+      <button onClick={() => setFormStage(formStage + 1)}>Next</button>
+      </>
+    );
+  }
+
+  if (formStage === 2) {
+    content = (
+      <>
+          {names.map((name) => (
+            <div>{name}</div>
+          ))}
+          <button onClick={() => setNames([...names,])}>Add Name</button>
+          <button onClick={() => setFormStage(formStage - 1)}>Prev</button>
+      </>
+    );
+  }
+
+  return <div className="App">{content}</div>;
+
+}
